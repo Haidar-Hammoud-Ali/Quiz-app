3d9adf724cf3f071b7cf8f7b9456651ba8ff4303
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-21 15:05:27 +0300

    Setting a welcoming message

--- JavaS/home.js
+++ JavaS/home.js
@@ -4,3 +4,5 @@ if (!loggin){
     alert ("You need to log in");
     window.location.href ="index.html";
 }
+
+document.getElementById("welcoming").innerText = `Welcome, ${loggin.email.split('@')[0]}!`;
