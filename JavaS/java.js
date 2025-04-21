2cf6a7b940ff4b66282fdb335781e68f4b1c82f7
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-21 15:48:00 +0300

    Adding Log in function

--- JavaS/java.js
+++ JavaS/java.js
@@ -18,8 +18,29 @@ if (!email || !password) {
   showTab('login');
 }
 
+function login (){
+  const email = document.getElementById("lemail").value.trim();
+  const password = document.getElementById("lpass").value;
 
-const user = users.find(u => u.email === email && u.password === password);
+  if (!email || !password) {
+    alert("Please enter both email and password.");
+    return;
+  }
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+  const user = users.find(u => u.email === email && u.password === password);
+  if (user) {
+    localStorage.setItem("loggedInUser", JSON.stringify(user));
+    alert(`Welcome back, ${email.split('@')[0]}!`);
+    if (email === "admin@quiz.com" && password === "admin123") {
+      window.location.href = "dashboard.html";
+    } else {
+      window.location.href = "home.html";
+    }
+  } else {
+    alert("Oops! Incorrect email or password.");
+  }
+}
+
 
 if (user) {
     localStorage.setItem("loggedInUser", JSON.stringify(user));
@@ -35,7 +56,7 @@ if (user) {
         alert("Retry another email or password.");
       }
       
-  function showTab(tab) {
-    document.getElementById('Login').style.display = tab === 'Login' ? 'block' : 'none';
-    document.getElementById('regist').style.display = tab === 'regist' ? 'block' : 'none';
-    }
+  // function showTab(tab) {
+  //   document.getElementById('Login').style.display = tab === 'Login' ? 'block' : 'none';
+  //   document.getElementById('regist').style.display = tab === 'regist' ? 'block' : 'none';
+  //   }
