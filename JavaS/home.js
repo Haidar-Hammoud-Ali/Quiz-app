6ad015d78dd7c9f7480fe8d7df06540f7ace28df
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-21 21:15:09 +0300

    Editing quiz list to take from local storage

--- JavaS/home.js
+++ JavaS/home.js
@@ -10,18 +10,16 @@ document.getElementById("welcoming").innerText = `Welcome, ${loggin.email.split(
 let quizzes = JSON.parse(localStorage.getItem("quizzes"));
 
 if (!quizzes || quizzes.length === 0) {
-  document.getElementById("quizList").innerText = "No quizzes yest.";
-}
-
-
-
-const quizList = document.getElementById("quizbdg");
-
+  quizList.innerText = "No quizzes yet.";
+} else {
 quizzes.forEach(quiz => {
-  const quizl = document.createElement("button");
-  quizl.innerText = quiz.title;
-  quizl.onclick = () => {
-    window.location.href = `quiz.html?quizId=${quiz.id}`;
+    const quizButton = document.createElement("button");
+    quizButton.innerText = quiz.title;
+    quizButton.onclick = () => {
+      
+      localStorage.setItem("currentQuizId", quiz.id);
+      window.location.href = "quiz.html";
   };
-  quizList.appendChild(quizl);
+    quizList.appendChild(quizButton);
 });
+}
