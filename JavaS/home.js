2b478cd60ae3d2a58f0a1fa9547981216b6ce5a8
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-21 15:14:53 +0300

    Generating some quizzes

--- JavaS/home.js
+++ JavaS/home.js
@@ -6,3 +6,52 @@ if (!loggin){
 }
 
 document.getElementById("welcoming").innerText = `Welcome, ${loggin.email.split('@')[0]}!`;
+
+let quizzes = JSON.parse(localStorage.getItem("quizzes"));
+if (!quizzes || quizzes.length === 0) {
+  quizzes = [
+    {
+      id: "quiz1",
+      title: "Quiz Title ",
+      questions: [
+        {
+          question: "Question 1",
+          options: ["choice 1", "choice 2", "choice 3"],
+          answer: "choice 2"
+        },
+        {
+          question: "Question 2",
+          options: ["choice 1", "choice 2", "choice 3"],
+          answer: "choice 3"
+        },
+        {
+          question: "Question 3",
+          options: ["choice 1", "choice 2", "choice 3"],
+          answer: "choice 1"
+        }
+      ]
+    },
+    {
+      id: "quiz2",
+      title: "Quiz 2 title",
+      questions: [
+        {
+          question: "Question 1",
+          options: ["choice 1", "choice 2", "choice 3"],
+          answer: "choice 3"
+        },
+        {
+          question: "Question 2",
+          options: ["choice 1", "choice 2", "choice 3"],
+          answer: "choice 3"
+        },
+        {
+          question: "Question 3",
+          options: ["choice 1", "choice 2", "choice 3"],
+          answer: "choice 2"
+        }
+      ]
+    }
+  ];
+  localStorage.setItem("quizzes", JSON.stringify(quizzes));
+}
