f1023f1b6142bf30e0336772a39d9d8d8f022fcf
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-21 14:38:48 +0300

    Adding showtab function

--- JavaS/java.js
+++ JavaS/java.js
@@ -8,12 +8,30 @@ if (!email || !password) {
   }
 }
 
-let users = JSON.parse(localStorage.getItem("users")) || [];
+
+const users = JSON.parse(localStorage.getItem("users")) || [];
 
 const user = users.find(u => u.email === email && u.password === password);
 
 if (user) {
     localStorage.setItem("loggedInUser", JSON.stringify(user));
     alert(`Welcome back, ${email.split('@')[0]}!`);
-  
-
+   
+    if (email === "log@admin.com" && password === "12345678") {
+        window.location.href = "dashboard.html";
+      } else {
+        window.location.href = "home.html";
+      }
+    }
+    else {
+        alert("Retry another email or password.");
+      }
+      
+function showTab(tab) {
+  document.getElementById('Login').style.display = tab === 'Login' ? 'block' : 'none';
+  document.getElementById('regist').style.display = tab === 'regist' ? 'block' : 'none';
+   }
+            
+      
+            
+      
