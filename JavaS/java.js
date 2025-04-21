24565cd1eb9b5a78bd8fde59bf793e85f4369f77
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-21 14:02:18 +0300

    Cheking for saved Users

--- JavaS/java.js
+++ JavaS/java.js
@@ -1,2 +1,11 @@
+function regist () {
 const email = document.getElementById("remail").value.trim();
 const password = document.getElementById("rpass").value;
+
+if (!email || !password) {
+    alert("Please fill in both email and password.");
+    return;
+  }
+}
+
+let user = JSON.parse(localStorage.getItem("users")) || [];
