1c2885e707db40129dfd6aaad9a17f676f0d1ce9
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-21 20:43:37 +0300

    Updating quiz storage

--- JavaS/home.js
+++ JavaS/home.js
@@ -8,61 +8,20 @@ if (!loggin){
 document.getElementById("welcoming").innerText = `Welcome, ${loggin.email.split('@')[0]}!`;
 
 let quizzes = JSON.parse(localStorage.getItem("quizzes"));
+
 if (!quizzes || quizzes.length === 0) {
-  quizzes = [
-    {
-      id: "quiz1",
-      title: "Quiz Title ",
-      questions: [
-        {
-          question: "Question 1",
-          options: ["choice 1", "choice 2", "choice 3"],
-          answer: "choice 2"
-        },
-        {
-          question: "Question 2",
-          options: ["choice 1", "choice 2", "choice 3"],
-          answer: "choice 3"
-        },
-        {
-          question: "Question 3",
-          options: ["choice 1", "choice 2", "choice 3"],
-          answer: "choice 1"
-        }
-      ]
-    },
-    {
-      id: "quiz2",
-      title: "Quiz 2 title",
-      questions: [
-        {
-          question: "Question 1",
-          options: ["choice 1", "choice 2", "choice 3"],
-          answer: "choice 3"
-        },
-        {
-          question: "Question 2",
-          options: ["choice 1", "choice 2", "choice 3"],
-          answer: "choice 3"
-        },
-        {
-          question: "Question 3",
-          options: ["choice 1", "choice 2", "choice 3"],
-          answer: "choice 2"
-        }
-      ]
-    }
-  ];
-  localStorage.setItem("quizzes", JSON.stringify(quizzes));
+  document.getElementById("quizList").innerText = "No quizzes yest.";
 }
 
-const quizList = document.getElementById("quizList");
+
+
+const quizList = document.getElementById("quizbdg");
 
 quizzes.forEach(quiz => {
-  const quizBtn = document.createElement("button");
-  quizBtn.innerText = quiz.title;
-  quizBtn.onclick = () => {
+  const quizl = document.createElement("button");
+  quizl.innerText = quiz.title;
+  quizl.onclick = () => {
     window.location.href = `quiz.html?quizId=${quiz.id}`;
   };
-  quizList.appendChild(quizBtn);
+  quizList.appendChild(quizl);
 });
