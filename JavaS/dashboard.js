944f082ee10a59b67382188139128d35fbc80727
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-21 21:02:08 +0300

    Identifying the users and quizzes lists

--- JavaS/dashboard.js
+++ JavaS/dashboard.js
@@ -4,3 +4,8 @@ if (!loggin || loggin.email !== "admin@q.com") {
   alert("Access to admin only.");
   window.location.href = "index.html";
 }
+
+const users = JSON.parse(localStorage.getItem("users")) || [];
+const quizzes = JSON.parse(localStorage.getItem("quizzes")) || [];
+
+const tableDiv = document.getElementById("userTable");
