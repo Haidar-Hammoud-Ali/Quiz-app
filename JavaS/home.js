76b3361111d18ba68eb75c0c74e3ef9d375b8f2e
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-27 15:22:58 +0300

    Performing prettier style

--- JavaS/home.js
+++ JavaS/home.js
@@ -1,7 +1,7 @@
-let loggin = localStorage.getItem("loggin");
-loggin = JSON.parse(loggin);
+let loggindata = localStorage.getItem("loggin");
+loggindata = JSON.parse(loggindata);
 
-if (loggin= null){
+if (loggindata=== null){
     alert ("You need to log in");
     window.location.href ="index.html";
 }
@@ -10,35 +10,34 @@ let welcomtxt = document.getElementById("welcoming");
 let namebefrat = loggin.email.split('@')[0];
 welcomtxt.innerText = "welcome," + namebefrat + "!";
 
-let quizzes = localStorage.getItem("quizzes");
-quizzes = json.parse (quizzes);
+let quizzesdata = localStorage.getItem("quizzes");
+quizzesdata = json.parse (quizzesdata);
 
 
-if (!quizzes || quizzes.length === 0) {
-  quizzes = [
-      {
-          id: "quiz1",
-          title: "First Quiz!",
-          questions: [
-              {
-                  question: "Question 1",
-                  options: ["choice 1", "choice 2", "choice 3", "choice 4"],
-                  answer: "choice 3"
-              },
-              {
-                  question: "Question 2",
-                  options: ["choice 1", "choice 2", "choice 3", "choice 4"],
-                  answer: "choice 2"
-              },
-              {
-                  question: "question 3",
-                  options: ["choice 1", "choice 2", "choice 3", "choice 4"],
-                  answer: "choice 1"
-              }
-          ]
-      }
-  ];
-  localStorage.setItem("quizzes", JSON.stringify(quizzes));
+if (quizzesdata===null || quizzesdata.length === 0) {
+quizzesdata = [
+{
+id: "quiz1",
+title: "First Quiz!",
+questions: [
+{  question: "Question 1",
+    options: ["choice 1", "choice 2", "choice 3", "choice 4"],
+        answer: "choice 3"
+      },
+       {
+ question: "Question 2",
+options: ["choice 1", "choice 2", "choice 3", "choice 4"],
+  answer: "choice 2"
+   },
+    {
+question: "question 3",
+options: ["choice 1", "choice 2", "choice 3", "choice 4"],
+answer: "choice 1"
+ }
+]
+}
+];
+localStorage.setItem("quizzes", JSON.stringify(quizzesdata));
 }
 
 const quizList = document.getElementById("quizbdg");
